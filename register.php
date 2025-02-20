b61e15bc14ec4b6ccb55a862c7273e50eafefeb8
crammer-web <mohamedyamni68@gmail.com>
2025-02-20 15:41:28 +0100

    Add files via upload

---
+++ register.php
@@ -0,0 +1,11 @@
+
+$name, 'email' => $email];
+
+   // Write the updated array back to the JSON file
+   file_put_contents('users.json', json_encode($users));
+}
+
+// Redirect back to index.html
+header('Location: index.html');
+exit();
+?>
